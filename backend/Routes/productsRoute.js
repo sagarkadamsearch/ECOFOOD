05fe6013f3ac8a5ff84cfba85682678ecf25572b
sagarkadamsearch <sagarkadamsearch@gmail.com>
2024-03-25 21:44:44 +0530

    update

--- backend/Routes/productsRoute.js
+++ backend/Routes/productsRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { productModel } = require('../models/product.model');
-const authMiddleware = require('../Middlewares/Auth.middleware');
+const { productModel } = require('../models/product.model.js');
+const authMiddleware = require('../Middlewares/Auth.middleware.js');
 const productRoute = express.Router();
 
 
